6ac37da60ebcc62ac482cf7214895a930452bec0
Nehme <nehme@Nehme.local>
2017-12-12 16:38:23 +0200

    update

--- fastmux/util/xserve.go
+++ fastmux/util/xserve.go
@@ -1,15 +1,12 @@
 package util
 
 import (
-	"bufio"
-	"bytes"
-	"net/http"
 	"net/url"
 	"os"
+	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/nehmeroumani/pill.go/helpers"
 	"github.com/valyala/fasthttp"
@@ -19,8 +16,9 @@ var staticFilesPath, uploadsPath, appVersion string
 var staticFilesURLPath, uploadsURLPath string
 var staticFilesFromCloud, uploadsFromCloud bool
 var cacheTTL = 60 * 60 * 24 * 30 * 6
+var fsUploadsHandler, fsStaticFilesHandler fasthttp.RequestHandler
 
-func InitXServe(StaticFilesPath string, StaticFilesURLPath string, StaticFilesFromCloud bool, UploadsPath string, UploadsURLPath string, UploadsFromCloud bool, AppVersion string) {
+func InitXServe(StaticFilesPath string, StaticFilesURLPath string, StaticFilesFromCloud bool, UploadsPath string, UploadsURLPath string, UploadsFromCloud bool, AppVersion string, opts ...bool) {
 	staticFilesPath = StaticFilesPath
 	staticFilesURLPath = StaticFilesURLPath
 	staticFilesFromCloud = StaticFilesFromCloud
@@ -28,6 +26,45 @@ func InitXServe(StaticFilesPath string, StaticFilesURLPath string, StaticFilesFr
 	uploadsURLPath = UploadsURLPath
 	uploadsFromCloud = UploadsFromCloud
 	appVersion = AppVersion
+	generateIndexPages := false
+	if opts != nil && len(opts) > 0 {
+		generateIndexPages = opts[0]
+	}
+	fsUploads := &fasthttp.FS{
+		Root:                 "/",
+		GenerateIndexPages:   generateIndexPages,
+		IndexNames:           []string{"index.html"},
+		Compress:             true,
+		AcceptByteRange:      true,
+		CompressedFileSuffix: ".gz",
+		PathRewrite: fasthttp.PathRewriteFunc(func(requestCtx *fasthttp.RequestCtx) []byte {
+			requestedFile := helpers.BytesToString(requestCtx.Path())
+			if strings.HasPrefix(requestedFile, "/public") {
+				requestedFile = requestedFile[len("/public"):]
+			}
+			requestedFile = requestedFile[len("/uploads"):]
+			return []byte(path.Join(uploadsPath, requestedFile))
+		}),
+	}
+	fsUploadsHandler = fsUploads.NewRequestHandler()
+	fsStaticFiles := &fasthttp.FS{
+		Root:                 "/",
+		IndexNames:           []string{"index.html"},
+		GenerateIndexPages:   generateIndexPages,
+		Compress:             true,
+		AcceptByteRange:      true,
+		CompressedFileSuffix: ".gz",
+		PathRewrite: fasthttp.PathRewriteFunc(func(requestCtx *fasthttp.RequestCtx) []byte {
+			requestedFile := helpers.BytesToString(requestCtx.Path())
+			if strings.HasPrefix(requestedFile, "/public") {
+				requestedFile = requestedFile[len("/public"):]
+			}
+			requestedFile = requestedFile[len("/static"):]
+			return []byte(path.Join(staticFilesPath, requestedFile))
+		}),
+	}
+	fsStaticFilesHandler = fsStaticFiles.NewRequestHandler()
+
 }
 
 func StaticFilesServe(requestCtx *fasthttp.RequestCtx) {
@@ -37,6 +74,7 @@ func StaticFilesServe(requestCtx *fasthttp.RequestCtx) {
 func UploadsServe(requestCtx *fasthttp.RequestCtx) {
 	xServe(requestCtx, uploadsPath, uploadsURLPath, uploadsFromCloud, false)
 }
+
 func xServe(requestCtx *fasthttp.RequestCtx, filesPath string, filesURLPath string, fromCloud bool, isStatic bool) {
 	requestedFile := helpers.BytesToString(requestCtx.Path())
 	if strings.HasPrefix(requestedFile, "/public") {
@@ -55,89 +93,82 @@ func xServe(requestCtx *fasthttp.RequestCtx, filesPath string, filesURLPath stri
 		}
 		requestedFile += "app_version=" + url.QueryEscape(appVersion)
 		requestCtx.Redirect(filesURLPath+requestedFile, 307)
-	} else {
-		f, err := os.Open(filesPath + filepath.FromSlash(requestedFile))
-		fileExtension := strings.ToLower(filepath.Ext(requestedFile))
-
-		if err == nil {
-			var contentType string
-			switch fileExtension {
-			case ".js":
-				contentType = "text/javascript"
-			case ".css":
-				contentType = "text/css"
-			case ".jpg":
-				contentType = "image/jpg"
-			case ".png":
-				contentType = "image/png"
-			case ".jpeg":
-				contentType = "image/jpeg"
-			case ".gif":
-				contentType = "image/gif"
-			case ".mp3":
-				contentType = "audio/mpeg"
-			case ".ogg":
-				contentType = "audio/ogg"
-			case ".woff":
-				contentType = "application/x-font-woff"
-			case ".ttf":
-				contentType = "application/font-sfnt"
-			case ".svg":
-				contentType = "image/svg+xml"
-			case ".eot":
-				contentType = "application/vnd.ms-fontobject"
-			case ".pdf":
-				contentType = "application/pdf"
-			case ".ipa":
-				contentType = "application/octet-stream"
-			case ".html":
-				contentType = "text/html"
-			case ".plist":
-				contentType = "application/x-plist"
-			case ".apk":
-				contentType = "application/vnd.android.package-archive"
-			default:
-				contentType = "text/plain"
-			}
-			requestCtx.SetContentType(contentType)
-			setResponseWriterCacheControl(requestCtx, cacheTTL)
-			cached := false
-			if fileInfo, infoErr := f.Stat(); infoErr == nil {
-				requestCtx.Response.Header.Set("Content-Length", strconv.FormatInt(fileInfo.Size(), 10))
-				lastModifiedTime := fileInfo.ModTime().UTC()
-				requestCtx.Response.Header.Set("Last-Modified", lastModifiedTime.Format(http.TimeFormat))
-				if modifiedSince := helpers.BytesToString(requestCtx.Request.Header.Peek("If-Modified-Since")); modifiedSince != "" {
-					if modifiedSinceTime, TPErr := time.Parse(http.TimeFormat, modifiedSince); TPErr == nil {
-						modifiedSinceTime = modifiedSinceTime.UTC()
-						if modifiedSinceTime.Equal(lastModifiedTime) {
-							requestCtx.SetStatusCode(304)
-							requestCtx.Response.SetBodyStream(bytes.NewReader([]byte{}), 0)
-							cached = true
-						}
-					}
-				}
-				if !cached {
-					buf := bufio.NewReader(f)
-					requestCtx.Response.SetBodyStream(buf, int(fileInfo.Size()))
-				}
-			}
+	} else if f, err := os.Open(filesPath + filepath.FromSlash(requestedFile)); err == nil {
+		defer f.Close()
+		fileInfo, infoErr := f.Stat()
+		if infoErr != nil {
+			requestCtx.Redirect("/404", 307)
 		} else {
-			requestCtx.SetStatusCode(404)
-			requestCtx.Response.SetBodyStream(bytes.NewReader([]byte(http.StatusText(404))), len([]byte(http.StatusText(404))))
+			if requestCtx.IsHead() && !fileInfo.IsDir() {
+				fileExtension := strings.ToLower(filepath.Ext(requestedFile))
+				if err == nil {
+					var contentType string
+					switch fileExtension {
+					case ".js":
+						contentType = "text/javascript"
+					case ".css":
+						contentType = "text/css"
+					case ".jpg":
+						contentType = "image/jpg"
+					case ".png":
+						contentType = "image/png"
+					case ".jpeg":
+						contentType = "image/jpeg"
+					case ".gif":
+						contentType = "image/gif"
+					case ".mp3":
+						contentType = "audio/mpeg"
+					case ".ogg":
+						contentType = "audio/ogg"
+					case ".mp4":
+						contentType = "video/mp4"
+					case ".avi":
+						contentType = "video/x-msvideo"
+					case ".wmv":
+						contentType = "video/x-ms-wmv"
+					case ".3gp":
+						contentType = "video/3gpp"
+					case ".mov":
+						contentType = "video/quicktime"
+					case ".mkv":
+						contentType = "video/x-matroska"
+					case ".woff":
+						contentType = "application/x-font-woff"
+					case ".ttf":
+						contentType = "application/font-sfnt"
+					case ".svg":
+						contentType = "image/svg+xml"
+					case ".eot":
+						contentType = "application/vnd.ms-fontobject"
+					case ".pdf":
+						contentType = "application/pdf"
+					case ".ipa":
+						contentType = "application/octet-stream"
+					case ".html":
+						contentType = "text/html"
+					case ".plist":
+						contentType = "application/x-plist"
+					case ".apk":
+						contentType = "application/vnd.android.package-archive"
+					default:
+						contentType = "text/plain"
+					}
+					requestCtx.SetContentType(contentType)
+					fileSize := fileInfo.Size()
+					requestCtx.Response.Header.Set("Content-Length", strconv.FormatInt(fileSize, 10))
+					requestCtx.Response.Header.Set("Accept-Ranges", "bytes")
+					requestCtx.Write([]byte{})
+				}
+			} else {
+				requestCtx.Request.SetRequestURIBytes(requestCtx.Path())
+				if isStatic {
+					fsStaticFilesHandler(requestCtx)
+				} else {
+					fsUploadsHandler(requestCtx)
+				}
+			}
 		}
-	}
-}
-func setResponseWriterCacheControl(requestCtx *fasthttp.RequestCtx, TLL int, opts ...bool) {
-	if TLL > 0 {
-		private := false
-		if opts != nil && len(opts) > 0 {
-			private = opts[0]
-		}
-		cacheControl := ""
-		if private {
-			cacheControl += "private, "
-		}
-		cacheControl += "max-age=" + strconv.Itoa(TLL)
-		requestCtx.Response.Header.Set("Cache-Control", cacheControl)
+	} else {
+		requestCtx.Redirect("/404", 307)
 	}
 }
